bbcbbc7feef8ce544da98bebba2860cbf048e698
mariacastellano <105322520+mariacastellano@users.noreply.github.com>
2023-10-22 23:21:55 -0300

    index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Chat app listening on port 3000');
+});
